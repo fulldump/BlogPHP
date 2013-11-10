1e44a58ae0a399f07876685d81ff82120da11bba
FullDump <gerardooscarjt@gmail.com>
2013-11-10 17:29:57 +0100

    Añadir la clase 'Database' para conectarnos con MySQL

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ Cosas buenas:
 * Permite almacenar y servir una gran cantidad de artículos
 * El diseño es muy simple
 * Todas las consultas a base de datos pasan a través de un único punto (la clase Database) 
+* Usa UTF-8 para la codificación de las conexiones con la base de datos
 
 Cosas malas:
 
@@ -36,3 +37,13 @@ Cosas malas:
 * No tiene interfaz de edición
 * No tiene tags
 * No utiliza una capa (tipo ORM) para acceder a la base de datos
+
+
+## ¿Cómo se usa UTF-8 para la codificación de las conexiones con la base de datos?
+
+La clase Database.class.php tiene un método estático privado en el que se establece la conexión con la base de datos. Justo después de establecer la conexión, le decimos que vamos a usar UTF-8:
+
+```
+mysql_query("SET NAMES 'utf8'");
+```
+
---
+++ class/Database.class.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Database
+ * El objetivo de esta clase es abstraer la conexión de la base de datos, lo que tiene
+ * algunas ventajas como despreocuparnos de las credenciales de acceso.
+ *
+ * Uso típico:
+ *     $rows = Database::query("SELECT * FROM `mi_tabla`");
+*/
+
+require_once('class/Config.class.php');
+
+class Database {
+
+	private static $connection = null;
+	private static $n = 0;
+
+	public static function query($sql) {
+		self::$n++;
+		$result =  mysql_query($sql);
+		if (mysql_errno()) {
+			echo mysql_error();
+		}
+		return $result;
+	}
+
+	public static function getN() {
+		return self::$n;
+	}
+
+	public static function connect() {
+		if (null != self::$connection) {
+			self::$connection = mysql_connect(
+				Config::get('database_host'),
+				Config::get('database_user'),
+				Config::get('database_pass')
+			);
+			mysql_select_db(Config::get('database_name'));
+			mysql_query("SET NAMES 'utf8'");
+		}
+	}
+	
+}
